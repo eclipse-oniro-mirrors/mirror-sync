6040c64488c5343d2bdf6ce32dcf5345289fe38f
Stefan Schmidt <stefan.schmidt@huawei.com>
2023-09-28 09:40:37 +0200

    main.yml: spread out sync into 8 parallel jobs
    This helps to speed up the whole sync process and stays within the 6h
    timeout for runners. An initial job is used to gather the current list
    of repos. To only retrieve the repo list instead of doing a full sync
    we are using our own modified fork of the hub-mirror-action for now.
    
    A small python script is used to handle splitting up the repo list in
    chunks and also doing some processing to remove known-problematic repos.
    They will be back once we have the issues around them debugged and
    fixed.
    
    Signed-off-by: Stefan Schmidt <stefan.schmidt@huawei.com>

---
+++ .github/workflows/split-list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from math import ceil
+import sys
+
+iput = sys.stdin.read()
+
+lst = iput.split(",")
+# Remove hiviewdfx_hisysevent as mapping it takes hours, to be debugged
+lst.remove("hiviewdfx_hisysevent")
+# Remove hiviewdfx_hitrace as a clone of this repo can take up to 5h
+lst.remove("hiviewdfx_hitrace")
+
+chunks = 8
+n = 1
+size = ceil(len(lst) / chunks)
+
+while lst:
+    chunk, lst = lst[:size], lst[size:]
+    filename = 'repo_list' + str(n) + '.txt'
+    repos = ','.join(chunk)
+    print("Filename for output: %s" % filename)
+    print("List of repos to write: %s" % repos)
+    with open(filename, 'w', encoding="utf-8") as text_file:
+        text_file.write("%s" % repos)
+    n = n+1
