fa4b7f5f8c6bf30984d178ce504c248940d322c7
Stefan Schmidt <stefan.schmidt@huawei.com>
2024-04-10 10:33:41 +0200

    Remove all hiviewdfx* repos from list to test speedup
    Signed-off-by: Stefan Schmidt <stefan.schmidt@huawei.com>

--- .github/workflows/split-list.py
+++ .github/workflows/split-list.py
@@ -6,10 +6,22 @@ import sys
 iput = sys.stdin.read()
 
 lst = iput.split(",")
-# Remove hiviewdfx_hisysevent as mapping it takes hours, to be debugged
-lst.remove("hiviewdfx_hisysevent")
-# Remove hiviewdfx_hitrace as a clone of this repo can take up to 5h
+# Remove hiviewdfx_* as mapping it takes hours, to be debugged
+lst.remove("hiviewdfx_hiview_lite")
+lst.remove("hiviewdfx_hiview")
+lst.remove("hiviewdfx_hilog_lite")
+lst.remove("hiviewdfx_hilog")
+lst.remove("hiviewdfx_hievent_lite")
+lst.remove("hiviewdfx_hidumper_lite")
+lst.remove("hiviewdfx_hidumper")
+lst.remove("hiviewdfx_hicollie")
+lst.remove("hiviewdfx_hichecker")
+lst.remove("hiviewdfx_hiappevent")
+lst.remove("hiviewdfx_faultloggerd")
+lst.remove("hiviewdfx_blackbox")
+lst.remove("hiviewdfx_blackbox_lite")
 lst.remove("hiviewdfx_hitrace")
+lst.remove("hiviewdfx_hisysevent")
 
 chunks = 8
 n = 1
