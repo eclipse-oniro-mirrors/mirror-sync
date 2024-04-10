a8b81ea5bfd8aec471d3bb2f3b4dcc4fdac2ace1
Stefan Schmidt <stefan.schmidt@huawei.com>
2024-04-10 10:33:41 +0200

    Update list of problematic repos and remove them from static list
    Repos with problems are sorted by problems now. Instead of using the
    blacklist functionality of hub-mirror-action (which would already
    process parts of it) we remove them already from the static list
    before splitting into chunks.
    
    Signed-off-by: Stefan Schmidt <stefan.schmidt@huawei.com>

--- .github/workflows/split-list.py
+++ .github/workflows/split-list.py
@@ -6,10 +6,39 @@ import sys
 iput = sys.stdin.read()
 
 lst = iput.split(",")
-# Remove hiviewdfx_hisysevent as mapping it takes hours, to be debugged
-lst.remove("hiviewdfx_hisysevent")
-# Remove hiviewdfx_hitrace as a clone of this repo can take up to 5h
-lst.remove("hiviewdfx_hitrace")
+
+# Repos with files over 100 MB -> error: GH001: Large files detected
+lst.remove("developtools_profiler")
+lst.remove("developtools_smartperf_host")
+lst.remove("device_soc_rockchip")
+lst.remove("global_i18n")
+lst.remove("third_party_mindspore")
+
+# error: GH008: Your push referenced at least 1 unknown Git LFS object
+lst.remove("device_board_hihope")
+
+# curl 92 HTTP/2 stream 5 was not closed cleanly: INTERNAL_ERROR (err 2)
+lst.remove("multimedia_av_codec")
+lst.remove("kernel_linux_5.10")
+lst.remove("third_party_llvm-project")
+lst.remove("kernel_linux_4.19")
+lst.remove("kernel_linux")
+lst.remove("third_party_typescript")
+
+# send-pack: unexpected disconnect while reading sideband packet
+lst.remove("third_party_vk-gl-cts")
+
+# fatal: destination path '/github/workspace/hub-mirror-cache/update_updater' already exists and is not an empty directory.'
+lst.remove("update_updater")
+
+# FileNotFoundError('[Errno 2] No such file or directory: '/github/workspace/hub-mirror-cache/third_party_noto-cjk'')
+lst.remove("third_party_noto-cjk")
+
+# LFS upload to github failed
+lst.remove("xts_acts")
+
+# Timeout gitee clone
+lst.remove("docs")
 
 chunks = 8
 n = 1
