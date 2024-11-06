14702b68e73a976e6c168ba83f2289fee7409dc0
Francesco Pham <francesco.pham@huawei.com>
2024-11-06 21:22:32 +0100

    remove blacklisting of working repos,
    - remove blacklisting of working repos
    - remove scheduling of attempt_mirror_repos.yml
    
    Signed-off-by: Francesco Pham <francesco.pham@huawei.com>

--- .github/workflows/split-list.py
+++ .github/workflows/split-list.py
@@ -15,23 +15,16 @@ lst.remove("device_soc_rockchip")
 lst.remove("global_i18n")
 lst.remove("third_party_mindspore")
 lst.remove("multimedia_audio_framework")
+lst.remove("multimedia_av_codec")
 
 # error: GH008: Your push referenced at least 1 unknown Git LFS object
 lst.remove("device_board_hihope")
-
-# curl 92 HTTP/2 stream 5 was not closed cleanly: INTERNAL_ERROR (err 2)
-lst.remove("multimedia_av_codec")
-lst.remove("kernel_linux_5.10")
-lst.remove("third_party_typescript")
 
 # send-pack: unexpected disconnect while reading sideband packet
 lst.remove("third_party_vk-gl-cts")
 
 # fatal: destination path '/github/workspace/hub-mirror-cache/update_updater' already exists and is not an empty directory.'
 lst.remove("update_updater")
-
-# FileNotFoundError('[Errno 2] No such file or directory: '/github/workspace/hub-mirror-cache/third_party_noto-cjk'')
-lst.remove("third_party_noto-cjk")
 
 # LFS upload to github failed
 lst.remove("xts_acts")
